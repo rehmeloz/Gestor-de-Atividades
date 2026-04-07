7810c7208782ec43629f63d29fb7a552c36dbc41
rehmeloz <renatamelo.pro@outlook.com>
2026-04-07 19:48:57 -0300

    adiciona dbcontext

---
+++ pro-atividade-app.Server/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using pro_atividade_app.Server.Models;
+using System.Data.Entity;
+
+namespace pro_atividade_app.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Atividade> Atividades { get; set; }
+    }
+}
