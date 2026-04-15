6d639d773912d775481960531ee77840fea9727d
rehmeloz <renatamelo.pro@outlook.com>
2026-04-15 19:26:00 -0300

    adiciona novas propriedades

--- pro-atividade-app.Server/Models/Atividade.cs
+++ pro-atividade-app.Server/Models/Atividade.cs
@@ -2,5 +2,18 @@
 
 public class Atividade
 {
-    public int id { get; set; }
+    public int Id { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public Prioridade Prioridade { get; set; }
+
+    //public Atividade() { }
+
+    //public Atividade(int id)
+    //{
+    //    Id = id;
+    //}
 }
